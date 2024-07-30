b412206dbe08141804ddd90178151d34758e79fb
Srujan Reddy <srjnreddy33@gmail.com>
2024-07-30 09:48:41 -0500

    Made http server for getdeck and deal functions

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getDeck(w http.ResponseWriter, r *http.Request) {
 	cards := newDeck()
 	cards.shuffle()
-	cards.print()
+	fmt.Printf("got request to getDeck \n")
+	io.WriteString(w, cards.toString())
+}
+
+func Deal(w http.ResponseWriter, r *http.Request) {
+	cards := newDeck()
+	cards.shuffle()
+	dealtCards, _ := deal(cards, 2)
+	fmt.Printf("got request to Deal \n")
+	io.WriteString(w, dealtCards.toString())
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Nothing on /, use /deal or /getdeck\n")
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/deal", Deal)
+	http.HandleFunc("/getdeck", getDeck)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+	// cards := newDeck()
+	// cards.shuffle()
+	// cards.print()
 }
